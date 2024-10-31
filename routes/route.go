ba597d1457afa6c37c5c2e7d9fa3e5451f5cad8b
chenjh <396814275@.com>
2024-10-31 11:43:33 +0800

    nothing

--- routes/route.go
+++ routes/route.go
@@ -31,6 +31,7 @@ func Setup(mode string) *gin.Engine {
 		v1.GET("/community", controllers.CommunityHandler)
 		v1.GET("/community/:id", controllers.CommunityDetailHandler)
 		v1.POST("/post", controllers.CreatePostHandler)
+		v1.GET("/post:id", controllers.CreatePostHandler)
 	}
 
 	r.NoRoute(func(c *gin.Context) {
